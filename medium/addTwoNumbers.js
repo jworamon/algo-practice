13f119c62b47031f6d72d23923dc7e5eba8913b2
Woramon Jamjod <jworamon@gmail.com>
2022-05-22 13:42:07 -0400

    addTwoNumbers

--- medium/addTwoNumbers.js
+++ medium/addTwoNumbers.js
@@ -5,7 +5,12 @@ class ListNode {
     }
 }
 
-const addTwoNumbers = (list1, list2) => {
+// if list is giving the least significant digit as head
+// i.e.     l1: 3 -> 4 -> 2 -> 7 (=7243)
+//          l2: 4 -> 6 -> 5 (=564)
+// return   l3: 7 -> 0 -> 8 -> 7 (=7807)
+
+const addTwoNumbers1 = (list1, list2) => {
     const resultHead = new ListNode(0);
     let current = resultHead;
     let addToNext = 0;
@@ -32,4 +37,37 @@ const addTwoNumbers = (list1, list2) => {
         current.next = new ListNode(1);
     }
     return resultHead.next;
+}
+
+// if list is giving the most significant digit as head
+// i.e.     l1: 7 -> 2 -> 4 -> 3 (=7243)
+//          l2: 5 -> 6 -> 4 (=564)
+// return   l3: 7 -> 8 -> 0 -> 7 (=7807)
+
+const addToNumbers2 = (list1, list2) => {
+    const stack1 = [];
+    const stack2 = [];
+    while (list1) {
+        stack1.push(list1.val);
+        list1 = list1.next;
+    }
+    while (list2) {
+        stack2.push(list2.val);
+        list2 = list2.next;
+    }
+
+    let list3 = new ListNode(0);
+    while (stack1.length || stack2.length) {
+        let sum = 0;
+        let num1 = stack1.pop() || 0; // assign 0 if no more num in stack
+        let num2 = stack2.pop() || 0; 
+        sum = num1 + num2 + list3.val;
+        list3.val = sum % 10;
+        
+        let head = new ListNode(Math.floor(sum / 10)); // add 1 to head.val if sum is more than 10
+        head.next = list3;
+        list3 = head;
+    }
+
+    return list3.val === 0 ? list3.next : list3;
 }
